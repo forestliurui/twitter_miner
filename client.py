09789ba7b462681439dc809f7c4a9bcf1f40fc66
Rui Liu <rxl341@case.edu>
2016-04-04 00:40:27 -0400

    some modifications to client.py

--- client.py
+++ client.py
@@ -1,13 +1,22 @@
 import requests
 import json
+from urllib import urlencode
 
 def client():
 	s = requests.Session()
-	url = "http://localhost:8080?screen_names=forestliurui,TheDeenShow&count=3"
+	screen_names = ["forestliurui", "TheDeenShow", "BigSean"]
+	count = 5
+	params ={"screen_names": ",".join(screen_names), "count": count}
+	
+	url =  "http://localhost:8080?"
+	url += urlencode(params)
+
+	#url = "http://localhost:8080?screen_names=forestliurui,TheDeenShow&count=3"
 	r = s.get(url)
 	json_return = r.text
 	json_object = json.loads(json_return)
 	print json_object
+	print ""
 	url +="&cursor="+json_object["next_cursor"]
 	r = s.get(url)
         json_return = r.text
