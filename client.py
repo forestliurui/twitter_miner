24257c85d545f1e1954d93da3770139127ca28d6
Rui Liu <rxl341@case.edu>
2016-04-10 11:26:53 -0400

    add some exception handling to twitter miner

--- client.py
+++ client.py
@@ -1,74 +1,44 @@
+"""
+This is the client of twitter miner which will communicate with server program to extract desired information
+"""
+
 import requests
 import json
 from urllib import urlencode
 from twitter_miner import write2html
-from httplib import HTTPConnection
-PORT = 8080
-
-def client1():
-	host = "http://localhost"
-        connection = HTTPConnection(host, PORT)
-        screen_names = ["forestliurui", "BigSean"]
-        count = 5
-        params ={"screen_names": ",".join(screen_names), "count": count}
-
-        url =  "http://localhost:8080?"
-        url += urlencode(params)
 
 
-        #url = "http://localhost:8080?screen_names=forestliurui,TheDeenShow&count=3"
-        req = connection.request("GET", urlencode(params))
-	resp = connection.getresponse()
-	try:
-		if resp.status == 200:
-		
-	
-        		json_return = resp.read()
-		else:
-			raise Exception("Got Code: %d" % resp.status)
-	except Exception as e:
-		print "Could not get response: %s" % e
-	
-        write2html(json_return, "test1.html")
-
-        json_object = json.loads(json_return)
-
-
-def client():
+def client(screen_names, count):
+	#open an session with server program
 	s = requests.Session()
-	screen_names = ["forestliurui", "BigSean"]
-	count = 5
+	
 	params ={"screen_names": ",".join(screen_names), "count": count}
 	
 	url =  "http://localhost:8080?"
-	url += urlencode(params)
+	url += urlencode(params)   #get the complete URL
 
 
 	#url = "http://localhost:8080?screen_names=forestliurui,TheDeenShow&count=3"
-	r = s.get(url)
-	json_return = r.text
+	r = s.get(url) #Send GET request
+	json_return = r.text #extract requested json-type result
 	
+	#write the json-type result into an html file
 	write2html(json_return, "test1.html")
 	
+	#the following code tries to use the returned next_cursor to continue request
 	json_object = json.loads(json_return)
 	print json_object
-	"""
-	print json_object
-	print ""
-	
-	file_name = "123.html"
-	with open(file_name, "a+") as fp:
-		for status in json_object["statuses"]:
-			#fp.write(status["status"])
-			json.dump(status["status"],fp)
-	"""
+
 	url +="&cursor="+json_object["next_cursor"]
-	r = s.get(url)
+	r = s.get(url) #Send another GET request with cursor set as next_cursor from previous request
         json_return = r.text
         json_object = json.loads(json_return)
 	write2html(json_return, "test2.html")
 	#print json_object
-	#import pdb;pdb.set_trace()	
+	
 
 if __name__ == "__main__":
-	client()
+	#specify the screen names and count
+	screen_names = ["forestliurui", "BigSean"]
+        count = 5
+	client(screen_names, count)
--- twitter_miner.py
+++ twitter_miner.py
@@ -4,34 +4,37 @@ import warnings
 from html import HTML
 import json
 
+#suppress the potential warning message
 warnings.filterwarnings("ignore")
 
-def twitter_api_login():
+def twitter_api_login(): #Login
 	consumer_key = 'eOlWInbxWsb0GrFb7sPbcerfG'
 	consumer_secret = 'vYAwYaGhgV2PSikjUyYZbUFheDDvckYlKl5ArYLdzlYSju8vYk'
 	access_token = '716475678993072128-7kVKnlTYWw27l0SJefrUEGjtnAspi1B'
 	access_secret = 'f7OAtB4sCp24ABvBxTaMRqCuR8iMMTeLO4QzscwGNZ6J3'
- 
+ 	#access_secret = 'f7OAtB4sCp24ABvBxTaMRqCuR8iMMTeLO4QzscwGNZ6J4'
 	auth = OAuthHandler(consumer_key, consumer_secret)
 	auth.set_access_token(access_token, access_secret)
  
 	api = tweepy.API(auth)
 	return api
 
-def format_status(status):
-	#h = HTML(newlines = False)
-	#import pdb;pdb.set_trace()
+def format_status(status): 
+	"""
+	convert each raw status into the required html-like format
+	"""
 	entities_list= []
-	entity_type_list = ["user_mentions", "hashtags","urls"]
-	if "media" in status.entities:
+	entity_type_list = ["user_mentions", "hashtags","urls"] #the three most popular entity types
+	if "media" in status.entities: #add media into entity_type_list if it appears in this status
 		entity_type_list.append("media")
+	
 	for entity_type in entity_type_list:
 		for entity in  status.entities[entity_type]:
 			entity.update({"entity_type":entity_type})
 			entities_list.append(dict(entity))
 	if len(entities_list)!= 0:
 		entities_list.sort(key= lambda item:item["indices"][0], reverse= True)
-	#import pdb;pdb.set_trace()
+	#convert status into required html-like format according to different entity type	
 	status_formated = status.text
 	for entity in entities_list:
 		if entity["entity_type"] == "user_mentions":
@@ -46,14 +49,25 @@ def format_status(status):
 		status_formated = status_formated[0:int(entity["indices"][0])]+unicode(h)+status_formated[int(entity["indices"][1]):]
 	return status_formated
 
-
 def get_statuses(api, users, count, max_id = None):
+	"""
+	get the list of statuses as requested and the next_cursor/min_id
+	"""
 	statuses_list = []
+	
 	for user_name in users:
+	    try:
 		for status in tweepy.Cursor(api.user_timeline, screen_name=user_name, max_id =  (max_id - 1 if max_id else None)  ).items(count):
 			statuses_list.append(status)
-	statuses_list.sort(key = lambda x: x.id, reverse = True)
+	    except Exception as e:
+		#when failing to get statuses from user, print the error message
+		error_dict = json.loads(e.message)
+		for error_message in error_dict["errors"]:
+			print "Error Message occurs when trying to get statuses from user %s : %s" % (user_name, error_message["message"])
+	#get the count number of most recent statuses from users that are before max_id
+	statuses_list.sort(key = lambda x: x.id, reverse = True) 
 	top_statuses_list = statuses_list[0:count]
+	#get the minimum id/next cursor
 	if len(top_statuses_list) != 0:
 		min_id = min([status.id for status in top_statuses_list])
 	else:
@@ -61,34 +75,37 @@ def get_statuses(api, users, count, max_id = None):
 	return top_statuses_list, min_id
 
 def construct_json(statuses_list, next_cursor):
+	"""
+	convert statuses list and next_cursor into a dictionary which is consistent with the desired json format
+	"""
 	json_dict = {}
 	json_dict["next_cursor"] = str(next_cursor)
 	json_dict["statuses"] = []
 	
 	for status in statuses_list:
-		#import pdb;pdb.set_trace()
+		
 		json_dict["statuses"].append({"status":format_status(status), "user":status.user.screen_name})
 
 	return json_dict
 
 def twitter_miner(users, count, cursor):
-
-
+	"""
+	This is the main function of twitter miner
+	"""
+	#login to the twitter api
 	api=twitter_api_login()
-
-	#count = 5
-	#users=["forestliurui", "joebonsall", "TheDeenShow"]
-	#users = ["forestliurui"]
-	
-
-	
-	status, min_id = get_statuses(api, users, count, cursor)
-	#import pdb;pdb.set_trace()
-	json_dict = construct_json(status, min_id)
+	#get the list of statuses and next_cursor
+	status, next_cursor = get_statuses(api, users, count, cursor)
+	#convert the statuses and next_cursor into json_dict of dictionary format which is consistent with the desired json format
+	json_dict = construct_json(status, next_cursor)
+	#convert the json_dict into json_result which is of json type 
 	json_result = json.dumps(json_dict)
 	return json_result
 
 def write2html(json_result, outputfile_name):
+	"""
+	convert json result into html file
+	"""
 	result = json.loads(json_result)
 	
 	f= open(outputfile_name,'w')
@@ -99,9 +116,11 @@ def write2html(json_result, outputfile_name):
 	f.close()
 
 
-def test_case():
-        users = ["BigSean"]
-        count = 5
+def test_case(): 
+	#test function for this file
+        users = ["BigSeannihao", "forestliurui"]
+        #users = ["forestliurui"]
+	count = 5
 	cursor = None
         result = twitter_miner(users, count, cursor)
 	outputfile_name = "test_miner.html"
@@ -109,54 +128,8 @@ def test_case():
 	import pdb;pdb.set_trace()
 
 if __name__ == "__main__":
+	#invoke test function when this script is run from command line
 	test_case()
-
-"""
-status2, min_id = get_statuses(api, users, count, min_id)
-json_dict2 = construct_json(status2, min_id)
-
-num = 1
-for status in status1:
-	print(str(num)+": ")
-        print(status.text)
-        print(format_status(status)) 
-	num += 1
-for status in status2:
-        print(str(num)+": ")
-        print(status.text)
-        print(format_status(status))
-        num += 1
-
-
-import pdb;pdb.set_trace()
-
-num = 1
-min_id = None
-for status in tweepy.Cursor(api.user_timeline, screen_name="joebonsall").items(10):
-    # Process a single status
-    	print(str(num)+": ")
-	print(status.text)
-	print(format_status(status))
-	#import pdb;pdb.set_trace()
-	#print(status.next_cursor)
-	if min_id is None:
-		min_id = status.id
-	else:
-		min_id = min(min_id, status.id)
-	num+=1 
-
-for status in tweepy.Cursor(api.user_timeline, screen_name="joebonsall", max_id = min_id -1  ).items(4):
-    # Process a single status
-        print(str(num)+": ")
-        print(status.text)
-        #import pdb;pdb.set_trace()
-        #print(status.next_cursor)
-        if min_id is None:
-                min_id = status.id
-        else:
-                min_id = min(min_id, status.id)
-        num+=1
-"""
 
 
 
